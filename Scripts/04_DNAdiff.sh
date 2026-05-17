af7c8a7bd08d221cbb3cd32c54b39d5e024dc3a4
Aurelianachilengue <76072461+Aurelianachilengue@users.noreply.github.com>
2026-05-17 16:05:00 +0100

    Update 04_DNAdiff.sh

--- Scripts/04_DNAdiff.sh
+++ Scripts/04_DNAdiff.sh
@@ -14,6 +14,9 @@
 
 mkdir -p dnadiff_results
 
+# set number of CPUs (parallel jobs)
+CPUS=8
+
 # ----------------------------------------------------------
 # Pairwise all-vs-all DNAdiff comparisons
 # ----------------------------------------------------------
@@ -30,8 +33,16 @@ for g1 in *.fasta; do
       echo "Running DNAdiff: $s1 vs $s2"
 
       dnadiff "$g1" "$g2" \
-        -p dnadiff_results/${s1}_vs_${s2}
+        -p dnadiff_results/${s1}_vs_${s2} &
+
+      # control number of parallel jobs
+      while (( $(jobs -r | wc -l) >= CPUS )); do
+        sleep 1
+      done
 
     fi
   done
 done
+
+# wait for all jobs to finish
+
